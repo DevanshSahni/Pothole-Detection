d360766acad2b81b4b7a7ffde418038c40591097
anu0108 <108670241+anu0108@users.noreply.github.com>
2023-11-16 20:18:34 +0530

    db

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { loadModel } = require("./YOLOv8/script");
 const cors = require("cors");
+const mongoose=require("mongoose");
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -8,13 +9,17 @@ app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
 // app.use(express.json());
 
+require("dotenv").config();
 
-app.use(
+
+app.use( 
   cors({
     origin: true,
     credentials: true,
   })
-);
+); 
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.rnblsxi.mongodb.net/potholes`);
 
 app.post("/prediction", async (req, res) => {
   const frame = req.body.image;
@@ -22,5 +27,5 @@ app.post("/prediction", async (req, res) => {
 });
 
 app.listen(3001, () => {
-  console.log("Server started");
+  console.log("Server started"); 
 });
